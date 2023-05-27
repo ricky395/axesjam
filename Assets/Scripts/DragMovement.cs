c4a374501900c3e2e3c7ffbfeecaf522bae05b75
Ricardo Roldán <ricardoroldan@axesinmotion.com>
2023-05-27 15:31:22 +0200

    new assets

--- Assets/Scripts/DragMovement.cs
+++ Assets/Scripts/DragMovement.cs
@@ -7,7 +7,7 @@ public class DragMovement : MonoBehaviour
     [SerializeField]
     private float moveForce;
     [SerializeField]
-    private LayerMask tapLayer;
+    private string tapLayer;
     [SerializeField]
     private LineRenderer helperLine;
 
@@ -33,43 +33,57 @@ public class DragMovement : MonoBehaviour
         else if (Input.GetMouseButtonUp(0))
         {
             TapReleased();
-            ApplyForce();
         }
 
         if (isHoldingTap)
         {
-            Debug.DrawRay(tapPoint, releasePoint, Color.red);
-        }
+            GetHitPoint(ref releasePoint);
 
-        Debug.Log(isHoldingTap);
+            Vector3 rayPoint = new Vector3(releasePoint.x, releasePoint.y + 0.5f, releasePoint.z);
+            helperLine.SetPosition(1, rayPoint);
+        }
     }
 
     private void TapPressed()
     {
         isHoldingTap = true;
         GetHitPoint(ref tapPoint);
+
+        helperLine.enabled = true;
+        Vector3 rayPoint = new Vector3(tapPoint.x, tapPoint.y + 0.5f, tapPoint.z);
+        helperLine.SetPosition(0, rayPoint);
     }
 
     private void TapReleased()
     {
         isHoldingTap = false;
         GetHitPoint(ref releasePoint);
+
+        helperLine.enabled = false;
+        ApplyForce();
     }
 
     private void GetHitPoint(ref Vector3 hitPoint)
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit[] hits = Physics.RaycastAll(ray.origin, ray.direction * 10);
 
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 100, tapLayer))
+        for (int i = 0; i < hits.Length; i++)
         {
-            hitPoint = hit.point;
+            RaycastHit hit = hits[i];
+
+            if (LayerMask.LayerToName(hit.collider.gameObject.layer).Equals(tapLayer))
+            {
+                hitPoint = hit.point;
+                break;
+            }
         }
     }
 
     private void ApplyForce()
     {
         forceVector = tapPoint - releasePoint;
+        forceVector = forceVector.normalized * maxDragDistance;
 
         rb.AddForce(forceVector * moveForce, ForceMode.Impulse);
     }
