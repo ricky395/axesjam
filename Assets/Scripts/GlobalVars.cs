a318226664ed8893d7aeb03e23ded35714e83120
Ricardo Roldán <ricardoroldan@axesinmotion.com>
2023-05-28 13:51:31 +0200

    trail

--- Assets/Scripts/GlobalVars.cs
+++ Assets/Scripts/GlobalVars.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GlobalVars : MonoBehaviour
 {
     private static int firstLevel = 4;
-    public static int level = 10;
+    public static int level = 4;
 
     public static void ResetProgress()
     {
